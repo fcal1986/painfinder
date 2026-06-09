118d24b752f91e9d2b18422f44d530a0649c745b
fcal1986 <fab.calcagno@gmail.com>
2026-06-09 09:11:41 +0200

    Add files via upload

--- docs/config.js
+++ docs/config.js
@@ -3,6 +3,6 @@
 // Schreibrechte werden durch RLS in Supabase begrenzt. Niemals den
 // service_role key hier eintragen!
 window.PAINFINDER_CONFIG = {
-  SUPABASE_URL: "https://rqupvcrmugfrmutcirlu.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxdXB2Y3JtdWdmcm11dGNpcmx1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMDEzNTksImV4cCI6MjA5NTg3NzM1OX0.ajmnedUdrt-xp9vp3g-04CCkMYnNYgaz5foUIAkN0T4",
+  SUPABASE_URL: "https://DEIN-PROJEKT.supabase.co",
+  SUPABASE_ANON_KEY: "DEIN_ANON_KEY",
 };
